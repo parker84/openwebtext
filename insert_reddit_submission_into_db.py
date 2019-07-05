bd621ac1035cbe1ddd89950b98234a56209ceb81
bryparker <brydon.parker@mail.utoronto.ca>
2019-07-05 13:34:45 -0400

    bug fixes in db insertion

--- insert_reddit_submission_into_db.py
+++ insert_reddit_submission_into_db.py
@@ -19,6 +19,7 @@ class RedditToDb():
         self.engine = create_engine(engine)
         self.table_name = save_to_table
         self.cols = None
+        self.get_cols_currently_in_table()
 
     def insert_submission_into_db(self, submission, if_exists="append"):
         df = self.subm_to_df(submission)
@@ -31,24 +32,37 @@ class RedditToDb():
         if add_tldr_cols:
             sub_dict.update(get_tldr_and_clean_text(submission))
             if sub_dict["tldr_summary"] is not None:
-                sub_dict["len_tldr_summary"] = len(sub_dict["tldr_summary"].split(" "))
-            else: 
+                sub_dict["len_tldr_summary"] = len(
+                    sub_dict["tldr_summary"].split(" "))
+            else:
                 sub_dict["len_tldr_summary"] = 0
             if sub_dict["tldr_content"] is not None:
-                sub_dict["len_tldr_content"] = len(sub_dict["tldr_content"].split(" "))
+                sub_dict["len_tldr_content"] = len(
+                    sub_dict["tldr_content"].split(" "))
             else:
                 sub_dict["len_tldr_content"] = 0
         return pd.DataFrame(
             [sub_dict]
         )
-    
+
+    def get_cols_currently_in_table(self):
+        try:
+            df = pd.read_sql_query("select * from " + self.table_name + " limit 10",
+                                   self.engine)
+            self.curr_cols = list(df.columns)
+        except Exception as error:
+            print(error)
+            self.curr_cols = None
+
     def select_cols(self, sub_dict):
         if self.cols is None:
             sub_dict = {k: v for k, v in sub_dict.items()
-                    if k not in KEYS_TO_REMOVE_FROM_REDDIT_RESULT}
+                        if k not in KEYS_TO_REMOVE_FROM_REDDIT_RESULT}
             self.cols = sub_dict.keys()
-        else:
-            sub_dict = {k: v for k, v in sub_dict.items()
+            if self.curr_cols is not None:
+                self.cols = [col for col in self.cols
+                             if col in self.curr_cols]
+        sub_dict = {k: v for k, v in sub_dict.items()
                     if k in self.cols}
         return sub_dict
 
